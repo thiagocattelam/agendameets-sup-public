c57b588ec417c6b3902f27bd57f90f70e6a8222e
Thiago <thiagocattelam123@gmail.com>
2026-06-22 13:12:21 -0300

    incluso lib do prisma

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  outputFileTracingIncludes: {
-    "/api/**/*": ["./lib/generated/prisma/**/*"],
-  },
-};
-
-module.exports = nextConfig;
